61d117c59739c369ef474e23d9d0abfbf56b2211
Ihar Bulaty <Ihar_Bulaty@epam.com>
2019-04-04 16:36:44 +0300

    feat:works under form

--- src/app/shared/form/form.ts
+++ src/app/shared/form/form.ts
@@ -1,18 +1,38 @@
-import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  OnDestroy,
+} from "@angular/core";
 import {
   FormGroup,
-  FormControl,
   Validators,
   FormBuilder,
+  AbstractControl,
 } from "@angular/forms";
 import { IRequest } from "../../common/entities/interfaces";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-form",
   templateUrl: "./form.html",
   styleUrls: ["./form.less"],
 })
-export class FormComponent implements OnInit {
+export class FormComponent implements OnInit, OnDestroy {
+  private sub: Subscription;
+
+  private validationMessage: any = {
+    Name: "",
+    LastName: "",
+  };
+  private validationMessages: any = [
+    { required: "Please enter your name." },
+    { minlength: "Must be longer than 1 characters." },
+    { pattern: "Must enclude only letters." },
+  ];
+
   @Input() public formRequestFields: IRequest;
   @Input() public addFlag: boolean;
   @Output() public changedVisibility: EventEmitter<boolean> = new EventEmitter<
@@ -24,17 +44,58 @@ export class FormComponent implements OnInit {
 
   public profileForm: FormGroup;
 
-  // tslint:disable-next-line:no-parameter-properties
   constructor(private fb: FormBuilder) {}
+
+  private setValidationMessage(c: AbstractControl, controlName: any): void {
+    this.validationMessage = "";
+    if ((c.touched || c.dirty) && c.errors) {
+      this.validationMessage = Object.keys(c.errors)
+        .map(key => this.validationMessages[controlName][key])
+        .join(" ");
+    }
+  }
 
   public ngOnInit(): void {
     this.createForm();
+    this.watchValueChanges();
+  }
+
+  public ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  public watchValueChanges(): void {
+    const nameControl: AbstractControl = this.profileForm.get([
+      this.formRequestFields.firstRow,
+    ]);
+    // const lastNameControl: AbstractControl = this.profileForm.get([
+    //   this.formRequestFields.secondRow,
+    // ]);
+    this.sub = nameControl.valueChanges.subscribe(value =>
+      this.setValidationMessage(nameControl, "Name")
+    );
+    // const subLastName: Subscription = lastNameControl.valueChanges.subscribe(value =>
+    //   this.setValidationMessage(lastNameControl, "Lastname")
+    // );
+    // this.sub.add(subName);
+    // this.sub.add(subLastName);
   }
 
   public save(): void {
     this.transferFormData.emit(this.profileForm);
     this.changedVisibility.emit(false);
     this.profileForm.reset();
+  }
+
+  public onBlur(): void {
+    const nameControl: AbstractControl = this.profileForm.get([
+      this.formRequestFields.firstRow,
+    ]);
+    // const lastNameControl: AbstractControl = this.profileForm.get([
+    //   this.formRequestFields.secondRow,
+    // ]);
+    this.setValidationMessage(nameControl, "Name");
+    // this.setValidationMessage(lastNameControl, "Lastname");
   }
 
   public createForm(): void {
