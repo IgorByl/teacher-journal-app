c552d9ce9faf2293200c321d6c7cb642f4606da2
Ihar Bulaty <Ihar_Bulaty@epam.com>
2019-04-08 13:09:55 +0300

    feat: add marks table

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, DoCheck, KeyValueDiffers, IterableDiffers } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  DoCheck,
+  KeyValueDiffers,
+  IterableDiffers,
+} from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { ListOfStudentsService } from "src/app/common/services";
 import { IStudent } from "src/app/common/entities/interfaces";
@@ -12,6 +18,8 @@ export class DashboardComponent implements OnInit, DoCheck {
   public subject: string;
   public students: IStudent[];
   public differ: any;
+  public date: Date;
+  public changes: any;
 
   constructor(
     private activateRoute: ActivatedRoute,
@@ -23,13 +31,16 @@ export class DashboardComponent implements OnInit, DoCheck {
 
   public ngOnInit(): void {
     this.getStudents();
-    this.differ = this.differs.find(this.students).create(null);
+    this.differ = this.differs
+      .find(this.students[0].subjects[this.subject].date)
+      .create(null);
   }
 
   public ngDoCheck(): void {
-    let changes: any = this.differ.diff(this.students);
+    let changes: any = this.differ.diff(
+      this.students[0].subjects[this.subject].date
+    );
     if (changes) {
-      console.log(changes);
       changes.forEachAddedItem(item => console.log("added", item));
       changes.forEachRemovedItem(item => console.log("Remove", item));
     }
@@ -39,5 +50,24 @@ export class DashboardComponent implements OnInit, DoCheck {
     this.listOfStudentsService
       .getStudents()
       .subscribe(students => (this.students = students));
+  }
+
+  public addDateColumn(): void {
+    this.changes = "p";
+    if (this.date) {
+      this.date.setDate(this.date.getDate() + 1);
+    } else {
+      this.date = new Date();
+    }
+    this.students[0].subjects[this.subject].date.push(
+      `${this.date.getDate()}.${this.date.getMonth()}`
+    );
+    this.students.forEach(item => {
+      item.subjects[this.subject].marks.push("");
+    });
+  }
+
+  public postData(): void {
+    console.log("post to server");
   }
 }
